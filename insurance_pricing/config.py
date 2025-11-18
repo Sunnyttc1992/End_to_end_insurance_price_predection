89d589aa373f80864b2b37c5c9169dbe27104bae
Sunnyttc <143631432+Sunnyttc1992@users.noreply.github.com>
2025-11-18 04:08:54 +0000

    update config

--- insurance_pricing/config.py
+++ insurance_pricing/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Paths
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA = DATA_DIR / "raw" / "insurance.csv"
 PROCESSED_DIR = DATA_DIR / "processed"
