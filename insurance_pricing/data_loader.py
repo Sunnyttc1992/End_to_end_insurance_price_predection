7c7e0d2fec22b71b16577d04637294a000a6b212
Sunnyttc <143631432+Sunnyttc1992@users.noreply.github.com>
2026-01-02 01:22:59 +0000

    reset

--- insurance_pricing/data_loader.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from .config import RAW_DATA
-
-def load_insurance_data() -> pd.DataFrame:
-    """
-    Load raw insurance data from CSV.
-    """
-    df = pd.read_csv(RAW_DATA)
-    return df
